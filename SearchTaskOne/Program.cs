1e01bfa25264c1af8bba88c31906a04f99091379
hend-shehata <hendshehata10@gmail.com>
2026-07-21 22:07:17 +0300

    Add project files.

---
+++ SearchTaskOne/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchTaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nFirstNumber = 50.666692;
+            double nSecondNumber = 20.3113515;
+
+            Console.WriteLine($" Percent {nFirstNumber}+ {nSecondNumber} is : {nFirstNumber + nSecondNumber:F2}");
+        }
+    }
+}
---
+++ TaskOne/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Islam's Carpet Cleaning Service Applicatoin ");
+            Console.WriteLine("The price of small carpet is : 25 $");
+            Console.WriteLine("The price of Large carpet is : 35 $");
+
+            Console.WriteLine("Please Enter The Number of small carpets");
+            int nNoSmallCarpets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Number of Large carpets");
+            int nNoLargeCarpets = Convert.ToInt32(Console.ReadLine());
+
+            int priceSmallCarpet = 25;
+            int priceLargeCarpet = 35;
+            double Taxes = 0.06;
+
+            int TotalPriceSmallCarpets = nNoSmallCarpets * priceSmallCarpet;
+            Console.WriteLine($"The Price of small carpet is : {TotalPriceSmallCarpets}");
+
+            int TotalPriceLargeCarpets = nNoLargeCarpets * priceLargeCarpet;
+            Console.WriteLine($"The Price of Large carpet is : {TotalPriceLargeCarpets}");
+
+            int TotalPrice = TotalPriceSmallCarpets + TotalPriceLargeCarpets;
+            Console.WriteLine($"Cost : {TotalPrice}");
+
+            double TotalTaxes = TotalPrice * Taxes;
+            Console.WriteLine($"Taxes is : {TotalTaxes:F2}");
+
+            double TotalPriceWithTaxes = TotalPrice + TotalTaxes;
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine($"The Total Cost with Taxes is : {TotalPriceWithTaxes}");
+
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
